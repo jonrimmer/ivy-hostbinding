413a4c529288d8794ca976a32eef0a64ba987917
Jon Rimmer <jon.rimmer@gmail.com>
2019-06-08 14:08:51 +0100

    Add bug demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ivy-hostbinding';
+export class AppComponent implements OnInit {
+  @HostBinding('style.color')
+  red: SafeStyle;
+
+  blue: SafeStyle;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    this.red = this.sanitizer.bypassSecurityTrustStyle('red');
+    this.blue = this.sanitizer.bypassSecurityTrustStyle('blue');
+  }
 }
